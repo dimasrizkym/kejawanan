cde0a14e0ca1a4f21bb67bb1b7564cdb44d1f04f
Dimas Rizky Maulana <dimasrizkym3@gmail.com>
2022-11-05 17:02:03 +0700

    create section ulasan

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,12 @@
-var swiper = new Swiper(".swiperKuliner", {
+new Swiper(".swiperKuliner", {
   slidesPerView: 'auto',
   spaceBetween: 24,
+});
+
+new Swiper(".swiperUlasan", {
+  spaceBetween: 24,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
